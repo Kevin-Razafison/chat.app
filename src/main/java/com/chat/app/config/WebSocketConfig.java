db4446dd3d2ee94136b546ace491a7ea26428be0
kevin <angelarknarem@gmail.com>
2026-01-19 20:33:04 +0300

    finalizing the chatapp

--- src/main/java/com/chat/app/config/WebSocketConfig.java
+++ src/main/java/com/chat/app/config/WebSocketConfig.java
@@ -12,7 +12,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOriginPatterns("htttp://localhost:5173")
+                .setAllowedOriginPatterns("htttp://localhost:8080")
                 .withSockJS();
     }
 
